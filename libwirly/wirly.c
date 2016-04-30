08175d12cbb0d291908bcae905e15b0559becd0d
JOHAN LANTZ <johan@hi>
2016-04-30 13:28:50 +0200

    removed precompiled header files

--- libwirly/wirly.c
+++ libwirly/wirly.c
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include "wirly.h"
 
 void test() {
--- wirly_cmd/wirly_cmd.cpp
+++ wirly_cmd/wirly_cmd.cpp
@@ -1,7 +1,4 @@
-// wirly_cmd.cpp : Defines the entry point for the console application.
-//
 
-//#include "stdafx.h"
 #include "wirly.h"
 
 int main()
