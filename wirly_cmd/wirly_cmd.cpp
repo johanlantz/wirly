20648288ee13bfb018e8e89886a5abf3ed716476
Johan Lantz <johan@hi>
2016-05-02 17:36:08 +0200

    support commandline

--- wirly_cmd/wirly_cmd.cpp
+++ wirly_cmd/wirly_cmd.cpp
@@ -6,12 +6,20 @@ static void log_cb(char* msg) {
 	printf(msg);
 }
 
-int main()
+int main(int argc, char** argv)
 {
 	wirly_config cfg = { &log_cb };
 	wirly_init(&cfg);
-	//wirly_decode_stream("C:\\users\\johan\\projects\\4500ilbcto-frs.pcap", "iLBC/8000", nullptr, nullptr);
 	printf(wirly_get_codecs());
+
+	if (argc == 1) {
+		wirly_decode_stream("C:\\users\\johan\\projects\\4500ilbcto-frs.pcap", "iLBC/8000", nullptr, nullptr);
+	}
+	else {
+		wirly_decode_stream((char*)argv[1], (char*)argv[2], nullptr, nullptr);
+	}
+		
+
 	getchar();
 
 
